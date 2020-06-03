c6a13e1177310cab0fa32cac60ebba60e7f57674
Max Reuter <maxreuter@unity3d.com>
2020-06-04 00:58:55 +0300

    add day filter

---
+++ src/day-filter/day-filter.js
@@ -0,0 +1,51 @@
+const addDays = require('date-fns/addDays')
+const format = require('date-fns/format')
+
+WEEKDAYS = {
+  SUNDAY: 0,
+  MONDAY: 1,
+  TUESDAY: 2,
+  WEDNESDAY: 3,
+  THURSDAY: 4,
+  FRIDAY: 5,
+  SATURDAY: 6,
+};
+
+
+function getWeekdayNumber(day) {
+  const number = WEEKDAYS[day.toUpperCase()];
+  return Number.isInteger(number) ? number : -1;
+}
+
+function getNextDateOfWeekday(date, weekday) {
+  const diff = (7 + weekday - date.getDay()) % 7;
+  const daysToAdd = diff > 0 ? diff : 7; // pick next week's version of today's weekday
+  return addDays(date, daysToAdd);
+}
+
+function getDate(str) {
+  const today = new Date();
+  const filter = str.toUpperCase();
+  if (filter === 'TODAY') {
+    return today;
+  }
+
+  if (filter === 'TOMORROW') {
+    return addDays(today, 1);
+  }
+
+  const weekday = getWeekdayNumber(filter);
+  return weekday > -1 ? getNextDateOfWeekday(today, weekday) : null;
+}
+
+function formatDate(date) {
+  return format(date, 'yyyy-MM-dd');
+}
+
+module.exports = {
+  WEEKDAYS,
+  formatDate,
+  getWeekdayNumber,
+  getNextDateOfWeekday,
+  getDate,
+}
