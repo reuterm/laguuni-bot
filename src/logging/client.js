7caf251729c78db6fa5047dd4ab2cdf9cfdb4fc6
Max Reuter <maxreuter@unity3d.com>
2023-05-10 17:01:59 +0200

    add severity for stackdriver logs

--- src/logging/client.js
+++ src/logging/client.js
@@ -1,8 +1,33 @@
 const winston = require("winston");
+const { LEVEL } = require('triple-beam');
+
+const SeverityLookup = {
+  'default': 'DEFAULT',
+  'silly': 'DEFAULT',
+  'verbose': 'DEBUG',
+  'debug': 'DEBUG',
+  'http': 'notice',
+  'info': 'info',
+  'warn': 'WARNING',
+  'error': 'ERROR',
+}
+
+const stackdriverSeverityFormat = winston.format((info) => ({
+  ...info,
+  // Add severity to your log
+  severity: SeverityLookup[info[LEVEL]] || SeverityLookup['default'],
+}));
+
+const formatters = [
+  winston.format.timestamp(),
+  // Add the format that supplements the JSON with severity
+  stackdriverSeverityFormat(),
+  winston.format.json(),
+];
 
 module.exports = winston.createLogger({
   level: "info",
-  format: winston.format.json(),
+  format: winston.format.combine(...formatters),
   transports: [
     new winston.transports.Console(),
   ],
